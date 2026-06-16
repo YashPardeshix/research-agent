7df17eb3fa17bcb89ac05a4717d07f688b85d1f8
Yash Pardeshi <ashpardeshi07@gmail.com>
2026-06-17 01:08:39 +0530

    nodes.py: completed the file

--- backend/nodes.py
+++ backend/nodes.py
@@ -7,8 +7,12 @@ def search_node(state: AgentState) -> AgentState:
     return {"urls_to_read": urls}
 
 def fetch_node(state:AgentState) -> AgentState:
+    local_findings = []
     for url in state["urls_to_read"]:
         content = fetch(url)
-    return {"findings": content}
+        if "Error" not in content:     
+            local_findings.append(content)
+    return {"findings": local_findings}
+
 
  
