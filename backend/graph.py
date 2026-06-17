e195e6d84de6bbf3fa789947b712ad4f063c0165
Yash Pardeshi <ashpardeshi07@gmail.com>
2026-06-18 02:18:54 +0530

    graphs.py: finished with the file

--- backend/graph.py
+++ backend/graph.py
@@ -1,1 +1,12 @@
-from langgraph.graph import StateGraph, END
+from langgraph.graph import StateGraph, START, END
+from state import AgentState
+from nodes import search_node, fetch_node
+
+workflow = StateGraph(AgentState)
+workflow.add_node("search", search_node)
+workflow.add_node("fetch", fetch_node)
+workflow.add_edge(START, "search")
+workflow.add_edge("search", "fetch")
+workflow.add_edge("fetch", END)
+
+app = workflow.compile()
