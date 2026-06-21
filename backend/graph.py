c62294e78f0cb76b856a237038482d2b207bbf3c
Yash Pardeshi <ashpardeshi07@gmail.com>
2026-06-21 17:11:14 +0530

    done with the project

--- backend/graph.py
+++ backend/graph.py
@@ -1,12 +1,14 @@
 from langgraph.graph import StateGraph, START, END
 from state import AgentState
-from nodes import search_node, fetch_node
+from nodes import search_node, fetch_node, synthesis_node
 
 workflow = StateGraph(AgentState)
 workflow.add_node("search", search_node)
 workflow.add_node("fetch", fetch_node)
+workflow.add_node("synthesis", synthesis_node)
 workflow.add_edge(START, "search")
 workflow.add_edge("search", "fetch")
-workflow.add_edge("fetch", END)
+workflow.add_edge("fetch", "synthesis")
+workflow.add_edge("synthesis", END)
 
 app = workflow.compile()
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from graph import app
+import json
 
 initial_state ={"query": "Latest advancements in fusion energy"}
 final_state = app.invoke(initial_state)
-print(final_state["findings"])
+finished_report=json.dumps(final_state["final_report"], indent=2)
+print(finished_report)
 
